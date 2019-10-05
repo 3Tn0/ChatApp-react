1bbe57bc3e26856b8aacad9aed8dc0e1e1012288
Jazz1464 <v.vl.stepanenko@gmail.com>
2019-10-05 12:38:49 +0700

    Добавлены базовые компоненты

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import './App.css';
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
 
-function App() {
-  return (
 
-  );
+class App extends React.Component {
+
+  render(){
+    
+    return(
+      <div className="app">
+      <RoomList />
+      <MessageList />
+      <SendMessageForm />
+      <NewRoomForm />
+      </div>
+    );
+  }
 }
 
 export default App;
