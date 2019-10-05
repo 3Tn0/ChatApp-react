4b3bd2b3bbd305b0738224d9602aefe3b84977bf
Jazz1464 <v.vl.stepanenko@gmail.com>
2019-10-05 16:39:37 +0700

    Сообщение выделено в отдельный компонент

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-class Message extends React.Component{
+function Message(props) {
 
-    render(){
-        return (
-            <div className="message">
-            </div>
-        )
-    }
+    return (
+        <div className="message">
+            <div className="message-username">{props.username}</div>
+            <div className="message-text">{props.text}</div>
+        </div>
+    )
 }
 
 export default Message
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,15 +1,14 @@
 import React from 'react'
 
+import Message from './Message'
+
 class MessageList extends React.Component {
     render() {
         return (
             <div className="message-list">
                 {this.props.messages.map((message, index) => {
                     return (
-                        <div key={index} className="message">
-                            <div className="message-username">{message.senderId}</div>
-                            <div className="message-text">{message.parts[0].payload.content}</div>
-                        </div>
+                        <Message username={message.senderId} text={message.parts[0].payload.content} key={index} />
                     )
                 })}
             </div>
